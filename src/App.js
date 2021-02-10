9906c1617b8dc8f637dbc4f5901f6ebac36c724c
Mainak Das <mainakdas104@gmail.com>
2021-02-10 12:13:55 +0530

    form submit, input handle

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from "react";
 import SearchBar from "./Components/SearchBar";
 
 class App extends Component {
+  onSearchSubmit = (term) => {
+    console.log(term);
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
