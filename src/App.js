cfa074f556065db9177de2a8897d35727e0c6e42
Mainak Das <mainakdas104@gmail.com>
2021-02-10 22:01:56 +0530

    async await

--- src/App.js
+++ src/App.js
@@ -3,19 +3,23 @@ import axios from "axios";
 import SearchBar from "./Components/SearchBar";
 
 class App extends Component {
-  onSearchSubmit = (term) => {
+  state = { images: [] };
+  onSearchSubmit = async (term) => {
     // console.log(term);
-    axios.get("https://api.unsplash.com/search/photos", {
+    const res = await axios.get("https://api.unsplash.com/search/photos", {
       headers: {
         Authorization: "Client-ID ONCkee_5wvu3cPqgTLATCOZHo4tOQOMQRtGq4YB3w2c",
       },
       params: { query: term },
     });
+    console.log(this);
+    this.setState({ images: res.data.results });
   };
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
