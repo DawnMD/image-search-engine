4647d279b581a82d5bcab8d146508afa477c2b0a
Mainak Das <mainakdas104@gmail.com>
2021-02-10 21:45:18 +0530

    integrated axios, made axios config

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./Components/SearchBar";
 
 class App extends Component {
   onSearchSubmit = (term) => {
-    console.log(term);
+    // console.log(term);
+    axios.get("https://api.unsplash.com/search/photos", {
+      headers: {
+        Authorization: "Client-ID ONCkee_5wvu3cPqgTLATCOZHo4tOQOMQRtGq4YB3w2c",
+      },
+      params: { query: term },
+    });
   };
   render() {
     return (
