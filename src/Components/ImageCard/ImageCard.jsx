2b48aa8a5fd09f9523c6ed37c3c191935e56beb3
Mainak Das <mainakdas104@gmail.com>
2021-02-11 22:43:47 +0530

    managed size

--- src/Components/ImageCard/ImageCard.jsx
+++ src/Components/ImageCard/ImageCard.jsx
@@ -1,11 +1,26 @@
-import React, { Component } from "react";
+import React, { Component, createRef } from "react";
 
 class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imgRef = createRef();
+  }
+
+  componentDidMount() {
+    this.imgRef.current.addEventListener("load", this.setSpan);
+  }
+
+  setSpan = () => {
+    const spans = Math.ceil(this.imgRef.current.clientHeight / 10);
+    this.setState({ spans });
+  };
+
   render() {
     const { alt_description, urls } = this.props.image;
     return (
-      <div>
-        <img src={urls.regular} alt={alt_description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imgRef} src={urls.regular} alt={alt_description} />
       </div>
     );
   }
